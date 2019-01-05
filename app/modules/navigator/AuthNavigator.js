813d4489a940370bfd2b9d5543776f30a0ebe329
graffme <daria.rucka@gmail.com>
2019-01-05 22:58:45 +0100

    Add navigation styles in auth scenes

--- app/modules/navigator/AuthNavigator.js
+++ app/modules/navigator/AuthNavigator.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { createStackNavigator} from 'react-navigation';
+import * as constants from '../../styles/constants.js';
 
 import LoginScreen from '../auth/scenes/Login.js';
 import SignupScreen from '../auth/scenes/Signup.js';
 
 const AuthStack = createStackNavigator({
-  Login: LoginScreen,
-  Signup: SignupScreen,
+  Login: {
+    screen: LoginScreen
+  },
+  Signup: {
+    screen: SignupScreen,
+    navigationOptions: {
+      title: 'Sign up',
+    },
+  },
 }, {
   initialRouteName: 'Login',
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: constants.colors.secondary,
+      elevation: 0,
+      borderBottomColor: constants.colors.secondary,
+    },
+    headerTintColor: constants.colors.primary,
+    headerBackTitle: null,
+  },
 })
 
 export default AuthStack;
