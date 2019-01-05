499cbc69c9e57b643e900b2b82f38ec029e55c0f
graffme <daria.rucka@gmail.com>
2019-01-05 19:03:10 +0100

    Add Signup scene in auth module

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 //import Home from './app/modules/home/scenes/Home.js';
-import Login from './app/modules/auth/scenes/Login.js';
+//import Login from './app/modules/auth/scenes/Login.js';
+import Signup from './app/modules/auth/scenes/Signup.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Login />
+      <Signup />
     );
   }
 }
---
+++ app/modules/auth/scenes/Signup.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import styles from '../../../styles/styles.js';
+import * as constants from '../../../styles/constants.js';
+import ThemeButton from '../../../components/Button/Button.js';
+import ThemeInput from '../../../components/TextInput/TextInput.js';
+
+const Signup = () => (
+  <View style={[styles.container, styles.darkBackground]}>
+    <Image source={require('../../../assets/img/logo.png')} style={{width: 50, height: 50, marginBottom: constants.grid.md}} />
+
+    <ThemeInput value="Email" style={{marginBottom: constants.grid.sm}} />
+    <ThemeInput value="Password" style={{marginBottom: constants.grid.xl}} />
+
+    <ThemeButton onPress={() => console.log("Signed up!")} text="Sign up" style={{marginBottom: constants.grid.sm}} />
+    <ThemeButton onPress={() => console.log("Signed up with FB!")} theme="facebook" text="Sign up with Facebook" style={{marginBottom: constants.grid.sm}}/>
+  </View>
+);
+
+export default Signup;
