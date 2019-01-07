43a3f0f5bf6cee588f1c974b2e63f082972eadae
graffme <daria.rucka@gmail.com>
2019-01-07 14:19:11 +0100

    Add redux packages and add Firebase config

---
+++ app/config/constants.js
@@ -0,0 +1,11 @@
+
+//To create ENV variables check out this -> https://stackoverflow.com/questions/48699820/how-do-i-hide-api-key-in-create-react-app
+//and then use your own api keys.
+export const FIREBASE_API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+export const FIREBASE_AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
+export const FIREBASE_DATABASE_URL = process.env.REACT_APP_FIREBASE_DATABASE_URL;
+export const FIREBASE_PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID;
+export const FIREBASE_STORAGE_BUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
+export const FIREBASE_MESSAGING_SENDER_ID = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
+
+export const FACEBOOK_APP_ID = process.env.REACT_APP_FACEBOOK_APP_ID;
---
+++ app/config/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import * as constants from "./constants"
+
+// Initialize Firebase
+const config = {
+    apiKey: constants.FIREBASE_API_KEY,
+    authDomain: constants.FIREBASE_AUTH_DOMAIN,
+    databaseURL: constants.FIREBASE_DATABASE_URL,
+    projectId: constants.FIREBASE_PROJECT_ID,
+    storageBucket: constants.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: constants.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const provider = new firebase.auth.FacebookAuthProvider();
+export const storage = firebase.storage();
