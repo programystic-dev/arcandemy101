ce8b8c55f3242e220cc7583526ace2ebece98d07
graffme <daria.rucka@gmail.com>
2019-02-04 16:57:17 +0100

    Change photo and title on the Home screen depending on a current chapter

---
+++ app/assets/img/mainImages.js
@@ -0,0 +1,7 @@
+const mainImages = {
+  main0: require('./main0.png'),
+  main1: require('./main1.png'),
+  main2: require('./main2.png'),
+}
+
+export default mainImages;
--- app/modules/home/scenes/Home.js
+++ app/modules/home/scenes/Home.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Image, Text, View } from 'react-native';
 import styles from '../../../styles/styles.js';
 import ThemeButton from '../../../components/Button/Button.js';
+import mainImages from '../../../assets/img/mainImages.js';
+import data from '../../../assets/data.json';
 
-const Home = () => (
-  <View style={[styles.container, styles.lightBackground]}>
-    <Image source={ require('../../../assets/img/cauldron.png') } style={[styles.bottomLg, styles.image]}/>
-    <Text style={[styles.textColor, styles.text16, styles.bottomSm]}>Continue reading...</Text>
-    <ThemeButton onPress={() => console.log("Taped!")} text="Altar"/>
-  </View>
-);
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default Home;
+  currentChapterIndex = (chapters) => {
+    let readChapters = chapters.filter((item) => item === false);
+    return readChapters.length - 1;
+  }
+
+  currentPhoto = (currentChapterIndex, images) => {
+    if (currentChapterIndex <= Object.keys(images).length - 1) {
+      return images[`main${currentChapterIndex}`];
+    } else {
+      return images['main0'];
+    }
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const { chapters } = this.props;
+    const currentChapterIndex = this.currentChapterIndex(chapters);
+    const currentChapterTitle = data.chapters[currentChapterIndex].title;
+
+    return(
+      <View style={[styles.container, styles.lightBackground]}>
+        <Image source={ this.currentPhoto(currentChapterIndex, mainImages) } style={[styles.bottomLg, styles.image]}/>
+        <Text style={[styles.textColor, styles.text16, styles.bottomSm]}>Continue reading...</Text>
+        <ThemeButton onPress={() => console.log('Move to Chapter')} text={currentChapterTitle}/>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    chapters: state.chapterReducer.chapters,
+  }
+}
+
+export default connect(mapStateToProps, null)(Home);
