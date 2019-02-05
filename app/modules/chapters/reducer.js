a73b42468af0eff807490854eb04df3ef714531d
graffme <daria.rucka@gmail.com>
2019-02-05 19:12:46 +0100

    Generate initial Chapters state dynamically for reducer

--- app/modules/chapters/reducer.js
+++ app/modules/chapters/reducer.js
@@ -1,14 +1,32 @@
 import * as types from './actionTypes';
+import data from '../../assets/data.json';
+
+//Generate initial states for Chapters and Pages based on JSON data
+const generateInitialChaptersState = (chapters) => {
+  const chaptersLocked = [ false ];
+  for (let i = 1; i < chapters.length; i++)  {
+    chaptersLocked.push(true);
+  };
+  return chaptersLocked;
+}
+
+const generateInitialPagesState = (chapters) => {
+  const pagesRead = [];
+  for (let i = 0; i < chapters.length; i++)  {
+    pagesRead.push([]);
+    for (let j = 0; j < chapters[i].pages.length; j++) {
+      pagesRead[i].push(false);
+    }
+  };
+  return pagesRead;
+}
 
 export const initialState = {
-  chapters: [ false, true, true ],
-  pages: [
-    [false, false, false, false],
-    [false, false, false, false, false, false, false],
-    [false, false, false, false, false]
-  ],
+  chapters: generateInitialChaptersState(data.chapters),
+  pages: generateInitialPagesState(data.chapters),
 };
 
+//Create Chapters reducer
 const chapterReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.CHAPTER_UNLOCKED:
